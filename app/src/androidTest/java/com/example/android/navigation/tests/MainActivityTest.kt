352dc895243f39410662233afec66ffdf04b042a
mcerezuela <marcus.cerezuela@gmail.com>
2024-09-10 15:09:48 +0200

    Update MainActivityTest.kt
    drawerAboutNAvigateBackToPLayTest

--- app/src/androidTest/java/com/example/android/navigation/tests/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/android/navigation/tests/MainActivityTest.kt
@@ -3,6 +3,7 @@ package com.example.android.navigation.tests
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.example.android.navigation.pageobjects.AboutScreen
 import com.example.android.navigation.pageobjects.DrawerScreen
 import com.example.android.navigation.pageobjects.PlayScreen
 import org.hamcrest.MatcherAssert.assertThat
@@ -12,27 +13,60 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
-class MainActivityTest: TestBase() {
+class MainActivityTest : TestBase() {
     private val playScreen = PlayScreen()
     private val drawerScreen = DrawerScreen()
+    private val aboutScreen = AboutScreen()
 
     @Test
     fun quizTitleIsReadyTest() {
         //playObject.clickOnStartQuiz()
         assertThat("Play button should be displayed", playScreen.isViewDisplayed(), `is`(true))
-        assertThat("Drawer should be available", playScreen.isNavigationDrawerButtonDisplayed(), `is`(true))
-        assertThat("Overflow menu button should be displayed", playScreen.isOverflowMenuButtonDisplayed(), `is`(true))
+        assertThat(
+            "Drawer should be available",
+            playScreen.isNavigationDrawerButtonDisplayed(),
+            `is`(true)
+        )
+        assertThat(
+            "Overflow menu button should be displayed",
+            playScreen.isOverflowMenuButtonDisplayed(),
+            `is`(true)
+        )
     }
+
     @Test
     fun drawerOptionsTest() {
         playScreen.clickOnNavigationDrawerButton()
         assertThat("Rules should be available", drawerScreen.isRulesOptionDisplayed(), `is`(true))
         assertThat("About should be available", drawerScreen.isAboutOptionDisplayed(), `is`(true))
     }
+
     @Test
-    fun drawerAboutTest() {
+    fun drawerAboutNAvigateBackToPLayTest() {
         playScreen.clickOnNavigationDrawerButton()
         drawerScreen.clickOnAboutButton()
+        assertThat(
+            "About Image should be Displayed",
+            aboutScreen.isAboutImageDisplayed(),
+            `is`(true)
+        )
+        assertThat(
+            "About  Text should be Displayed",
+            aboutScreen.isAboutTextDisplayed(),
+            `is`(true)
+        )
+        assertThat(
+            "About  Title should be Displayed",
+            aboutScreen.isAboutTitleDisplayed(),
+            `is`(true)
+        )
+        assertThat(
+            "About  Title return to main Displayed",
+            aboutScreen.isReturnToPageDisplayed(),
+            `is`(true)
+        )
+        aboutScreen.clickOnNavigateUpButton()
+        assertThat("Play button should be displayed", playScreen.isViewDisplayed(), `is`(true))
     }
 
 }
