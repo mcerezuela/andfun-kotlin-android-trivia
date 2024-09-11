caf91ed5bc9c06a666ec3918f3cada3f641eced2
mcerezuela <marcus.cerezuela@gmail.com>
2024-09-11 09:14:35 +0200

    Update GameScreen.kt
    added incorrect data

--- app/src/androidTest/java/com/example/android/navigation/pageobjects/GameScreen.kt
+++ app/src/androidTest/java/com/example/android/navigation/pageobjects/GameScreen.kt
@@ -1,6 +1,7 @@
 package com.example.android.navigation.pageobjects
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
@@ -10,26 +11,30 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
+
 import com.example.android.navigation.R
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
 
 class GameScreen : PageObjectBase() {
 
     data class QuestionCorrectAnswers(
         val text: String,
-        val answer: String
+        val answer: String,
+        val incorrect: String
     )
 
     private val questionCorrectAnswers: MutableList<QuestionCorrectAnswers> = mutableListOf(
-        QuestionCorrectAnswers(text = "What is Android Jetpack?", answer = "all of these"),
-        QuestionCorrectAnswers(text = "Base class for Layout?", answer = "ViewGroup"),
-        QuestionCorrectAnswers(text = "Layout for complex Screens?", answer = "ConstraintLayout"),
-        QuestionCorrectAnswers(text = "Pushing structured data into a Layout?", answer = "Data Binding"),
-        QuestionCorrectAnswers(text = "Inflate layout in fragments?", answer = "onCreateView"),
-        QuestionCorrectAnswers(text = "Build system for Android?", answer = "Gradle"),
-        QuestionCorrectAnswers(text = "Android vector format?", answer = "VectorDrawable"),
-        QuestionCorrectAnswers(text = "Android Navigation Component?", answer = "NavController"),
-        QuestionCorrectAnswers(text = "Registers app with launcher?", answer = "intent-filter"),
-        QuestionCorrectAnswers(text = "Mark a layout for Data Binding?", answer = "<layout>")
+        QuestionCorrectAnswers(text = "What is Android Jetpack?", answer = "all of these",incorrect = "libraries"),
+        QuestionCorrectAnswers(text = "Base class for Layout?", answer = "ViewGroup",incorrect = "ViewRoot"),
+        QuestionCorrectAnswers(text = "Layout for complex Screens?", answer = "ConstraintLayout",incorrect = "FrameLayout"),
+        QuestionCorrectAnswers(text = "Pushing structured data into a Layout?", answer = "Data Binding",incorrect = "OnClick"),
+        QuestionCorrectAnswers(text = "Inflate layout in fragments?", answer = "onCreateView",incorrect = "onInflateLayout"),
+        QuestionCorrectAnswers(text = "Build system for Android?", answer = "Gradle",incorrect = "Groyle"),
+        QuestionCorrectAnswers(text = "Android vector format?", answer = "VectorDrawable",incorrect = "AndroidVector"),
+        QuestionCorrectAnswers(text = "Android Navigation Component?", answer = "NavController",incorrect = "NavSwitcher"),
+        QuestionCorrectAnswers(text = "Registers app with launcher?", answer = "intent-filter",incorrect = "app-launcher"),
+        QuestionCorrectAnswers(text = "Mark a layout for Data Binding?", answer = "<layout>",incorrect = "<dbinding>")
     )
 
     private val questionImage = R.id.questionImage
@@ -59,6 +64,11 @@ class GameScreen : PageObjectBase() {
         clickOnSubmitButton()
     }
 
+    fun selectInCorrectAnswer() {
+        incorrectAnwser()
+        clickOnSubmitButton()
+    }
+
     fun clickOnSubmitButton() {
         onView(withId(submitButton)).perform(click())
     }
@@ -67,12 +77,24 @@ class GameScreen : PageObjectBase() {
         val question = getQuestionText()
         val answer = getCorrectAnswer(question)
         if (answer != null) {
-            selectCorrectRadioButton(answer)
+            selectRadioButton(answer)
+        }
+    }
+
+    private fun incorrectAnwser() {
+        val question = getQuestionText()
+        val answer = getIncorrectAnswer(question)
+        if (answer != null) {
+            selectRadioButton(answer)
         }
     }
 
     private fun getCorrectAnswer(question: String): String? {
         return questionCorrectAnswers.find { it.text == question }?.answer
+    }
+
+    private fun getIncorrectAnswer(question: String): String? {
+        return questionCorrectAnswers.find { it.text == question }?.incorrect
     }
 
     private fun getQuestionText(): String {
@@ -98,7 +120,7 @@ class GameScreen : PageObjectBase() {
         })
         return stringHolder[0] ?: "" // Return the text or an empty string
     }
-    private fun selectCorrectRadioButton(answer: String) {
+    private fun selectRadioButton(answer: String) {
         // Find the radio button within the RadioGroup that matches the answer text
         onView(
             allOf(
@@ -106,6 +128,21 @@ class GameScreen : PageObjectBase() {
                 withText(answer) // Matches the radio button with the answer text
             )
         ).perform(click()) // Click on the correct radio button
+    }
+
+
+    private fun atPosition(matcher: Matcher<View>, position: Int): TypeSafeMatcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                matcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && matcher.matches(parent) && view == parent.getChildAt(position)
+            }
+        }
     }
 
 }
